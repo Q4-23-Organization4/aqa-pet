4a969b7fea95e3834e9976f233ecf86429d87bb3
Aleksandr Tesliuk <philosopher680@gmail.com>
2023-12-20 21:47:52 +0200

    Commit for conflict creation

--- Homework_4/src/main/FirstTask.java
+++ Homework_4/src/main/FirstTask.java
@@ -1,5 +1,5 @@
 public class FirstTask {
     public static void main(String[] args) {
-        System.out.println("I don't know what to type, just free text.");
+        System.out.println("I don't know what to type, just free text (modified for conflict).");
     }
 }
