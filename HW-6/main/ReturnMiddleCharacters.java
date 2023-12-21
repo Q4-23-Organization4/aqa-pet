feb8130f3aa042a1501b66fbeea0f242d94795c4
Aleksandr Tesliuk <philosopher680@gmail.com>
2023-12-21 21:26:30 +0200

    Changes to the implementation of searching for two middle characters

--- HW-6/main/ReturnMiddleCharacters.java
+++ HW-6/main/ReturnMiddleCharacters.java
@@ -1,3 +1,5 @@
+import java.util.stream.Collectors;
+
 public class ReturnMiddleCharacters {
     public static void main(String[] args) {
         /*
@@ -8,8 +10,17 @@ public class ReturnMiddleCharacters {
 
         for (String word : words) {
             if (word.length() % 2 == 0) {
-                int middle = word.length() / 2;
-                String middleCharacters = word.substring(middle - 1, middle + 1);
+
+//                int middle = word.length() / 2;
+//                String middleCharacters = word.substring(middle - 1, middle + 1);
+
+                // Не костильна реалізація.))
+                String middleCharacters = word.chars()
+                        .mapToObj(c -> String.valueOf((char) c))
+                        .skip(word.length() / 2 - 1)
+                        .limit(2)
+                        .collect(Collectors.joining());
+
                 System.out.println("For string \"" + word + "\" result is: \"" + middleCharacters + "\"");
             } else {
                 System.out.println("String \"" + word + "\" does not have an even length.");
