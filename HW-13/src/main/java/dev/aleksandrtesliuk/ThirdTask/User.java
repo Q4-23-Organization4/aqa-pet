c2fb95999836ab0ba389d652e14fd0a1a1e3a24c
Aleksandr Tesliuk <philosopher680@gmail.com>
2024-05-12 23:50:58 +0300

    Third task

---
+++ HW-13/src/main/java/dev/aleksandrtesliuk/ThirdTask/User.java
@@ -0,0 +1,34 @@
+package dev.aleksandrtesliuk.ThirdTask;
+
+class User {
+    private final String firstName;
+    private final String secondName;
+    private final int age;
+
+    public User(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
