201e43e82809bb06b9cb982caa118d1de6b89936
Aleksandr Tesliuk <philosopher680@gmail.com>
2024-05-12 16:31:59 +0300

    Second task

---
+++ HW-9/src/main/java/dev/aleksandrtesliuk/SecondTask/BasicCalculator.java
@@ -0,0 +1,8 @@
+package dev.aleksandrtesliuk.SecondTask;
+
+class BasicCalculator extends Calculator {
+    @Override
+    public void specificFunction() {
+        System.out.println("Basic Calculator: No additional functions.");
+    }
+}
